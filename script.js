badd0bdee9bd3dc52a06673cb3779600393ae3cc
Jrmoura69 <111475680+Jrmoura69@users.noreply.github.com>
2024-01-01 17:03:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,15 @@ navCloseBtn.addEventListener("click", () => {
   nav.classList.remove("openNav");
 });
 
+var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
